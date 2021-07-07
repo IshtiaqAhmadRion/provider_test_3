bf439fbec79928a4d19c125e6e3a35bbb9a8ce83
IshtiaqAhmadRion <ishtiaq.ahmad5662@gmail.com>
2021-07-08 00:27:44 +0600

    submit button

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -8,17 +8,62 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
+  final nameKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text('PROVIDER TEST',
-        style: TextStyle(
-          color: Colors.white,
-        
-        ),),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text(
+            'PROVIDER TEST',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Form(
+              key: nameKey,
+              child: TextFormField(
+                decoration:
+                InputDecoration(
+                  hintText: 'Your Name',
+                  icon: Icon(Icons.person),
+                  labelText: 'Name*'),
+                validator: (String? value) {
+                  return (value != null && value.contains('@'))
+                      ? 'Do not use the @ char.'
+                      : null;
+                },
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            // ignore: deprecated_member_use
+            RaisedButton(
+              child: Text("Submit"),
+              onPressed: () {
+              nameKey.currentState!.validate();
+            })
+
+            // TextFormField(
+            //   decoration: const InputDecoration(
+            //     icon: Icon(Icons.person),
+            //     hintText: 'What do people call you?',
+            //     labelText: 'Name *',
+            //   ),
+            //   onSaved: (String? value) {
+            //     // This optional block of code can be used to run
+            //     // code when the user saves the form.
+            //   },
+            //   validator: (String? value) {
+            //     return (value != null && value.contains('@'))
+            //         ? 'Do not use the @ char.'
+            //         : null;
+            //   },
+            // )
+          ],
+        ));
   }
 }
